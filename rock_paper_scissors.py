28d2e1334a81609de8ae69721ba8f245aef69ff9
Hamza <hamzawritescode@gmail.com>
2025-03-24 06:56:44 +0400

    retry loop input

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,52 +1,43 @@
 
     # ------------------------------------------------ Rock, Paper, Scissors Game ----------------------------------------------------------- #
 
-    # --- Note: If you see things more than required in the code, it's because I'm trying to make the code more interactive and user-friendly, + I am experienced  --- #
 
+import random # Importing random module to generate random numbers for computer's choiceimport random
+status = True # initalize status variable to keep the game running
+print(f"Welcome to Rock, Paper, Scissors Game!")
 
-    # -------------------------------------------------------- Importing Modules & Welcome Message ------------------------------------------ #
+while status:
+    while player := input(f"Enter your choice: 1 for Rock ✊, 2 for Paper ✋, 3 for Scissors ✌️: "):
+        if player != "1" and player != "2" and player != "3":
+            print("Invalid Input! Please try again!")
+        else:
+            player = int(player)
+            break
 
-import random # Importing random module to generate random numbers for computer's choice
-print(f"Welcome to Rock, Paper, Scissors Game!") # Welcome message
+    computer = int(random.randint(1, 3)) # Computer Variable to store computer's choice using random.randint() function
 
-    # -------------------------------------------------------- Variables -------------------------------------------------------------------- #
-
-player = int(input(f"Enter your choice: 1 for Rock ✊, 2 for Paper ✋, 3 for Scissors ✌️: ")) # player variable to store player's choice
-computer = int(random.randint(1, 3)) # Computer Variable to store computer's choice using random.randint() function
-
-
-    # -------------------------------------------------------- Deciding the winner ------------------------------------------------------------------ #
-
-    # -------------------------------------------------------- Player Wins ------------------------------------------------------------------------ #
-
-if player == 1 and computer == 3:
-    print(f"Player Wins! Rock ✊ beats Scissors✌️!")
-
-elif player == 2 and computer == 1:
-    print(f"Player Wins! Paper✋ beats Rock ✊!")
-
-elif player == 3 and computer == 2:
-    print(f"Player Wins! Scissors✌️ beats Paper✋!")
-
-# -------------------------------------------------------- Computer Wins ---------------------------------------------------------------------- #
-
-elif player == 1 and computer == 2:
-    print(f"Computer Wins! Paper✋ beats Rock ✊!")
-
-elif player == 2 and computer == 3:
-    print(f"Computer Wins! Scissors✌️ beats Paper✋!")
-
-elif player == 3 and computer == 1:
-    print(f"Computer Wins! Rock ✊ beats Scissors✌️!")
-
-# -------------------------------------------------------- Tie ------------------------------------------------------------------------------- #
-
-elif player == computer:
-    print(f"It's a Tie!")
-
-# -------------------------------------------------------- Invalid Input For Player ---------------------------------------------------------------------- #
-
-else:
-    print(f"Invalid Input! Please try again!") # If player enters invalid input
-
-# -------------------------------------------------------- End of Rock, Paper, Scissors Game ----------------------------------------------------------- #
+    if player == 1 and computer == 3:
+        print(f"Player Wins! Rock ✊ beats Scissors✌️!")
+    elif player == 2 and computer == 1:
+        print(f"Player Wins! Paper ✋ beats Rock ✊!")
+    elif player == 3 and computer == 2:
+        print(f"Player Wins! Scissors ✌️  beats Paper✋!")
+    elif player == 1 and computer == 2:
+        print(f"Computer Wins! Paper ✋ beats Rock ✊!")
+    elif player == 2 and computer == 3:
+        print(f"Computer Wins! Scissors ✌️ beats Paper✋!")
+    elif player == 3 and computer == 1:
+        print(f"Computer Wins! Rock ✊ beats Scissors✌️!")
+    elif player == computer:
+        print(f"It's a tie!")
+    else:
+        print(f"Invalid Input! Please try again!")
+    
+    while status_response := input("Do you want to play again? (yes/no): ").lower():
+        if status_response == "yes":
+            break
+        elif status_response == "no":
+            status = False
+            break
+        else:
+            print("Invalid Input! Please try again!")
